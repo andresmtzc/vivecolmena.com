6d53a65b3bea88589afdc5c8b6391ad3de41f8b6
andresmtzc <andresmtzc@gmail.com>
2019-03-22 10:19:59 -0600

    1
    1

---
+++ web/9EPD_KY6qh7gC-zaNLjmO7w9PW5D8i_GfW2hweH0bA9fe1jJXnX1IyvhF2jtFRZLFQb3FA9tZeZ8FeJ3wQiyZQbUjRbujRSaeRBzdPuqZAuzO1FUiABkZWF3jAF8OcFzdP37OcNlOAs8dAv0SaBujW48Sagyjh90jhNlJyBq-AtljhN0Zem0ShmqOWi8jKG4fVl9IMMjgfMf.js
@@ -0,0 +1,12 @@
+if (!window.Typekit)
+    window.Typekit = {};
+window.Typekit.config = {
+    "a": "646866",
+    "f": "//use.typekit.net/c/6e1e7f/1w;din-condensed-web,2,ZwM:R:n4/{format}{/extras*}?3bb2a6e53c9684ffdc9a9aff135b2a629c74e59eae32629bb0e3457aa5caf19dc49b0dbacbb55edf2dcf80f579ca674d12ed590bf58fc5d522f06fe6822293b653b854b4c1ac3ffd0a8038deab2fd86fb01f06ba5e3ce5bd4bede9d4feff7e6281f55a",
+    "fi": [10875],
+    "fn": ["din-condensed-web", ["n4"]],
+    "ht": "tk",
+    "kt": "646866_5405e8dfe4b017ae41b45b77",
+    "p": "//p.typekit.net/p.gif?s=2&k=646866_5405e8dfe4b017ae41b45b77&ht=tk&h={host}&f=10875&a=646866&_={_}",
+    "ps": 2
+};
